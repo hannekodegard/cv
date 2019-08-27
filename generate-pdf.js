0614cebfff84e3f3b813607c0745ba9871aa39d7
Hanne Kyllo Ødegård <hanne.1998@live.no>
2019-08-27 18:47:20 +0200

    Run chrome in docker to generate pdf (OMG)

--- generate-pdf.js
+++ generate-pdf.js
@@ -1,12 +1,14 @@
-const puppeteer = require('puppeteer')
+var puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
+  var browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
+  var page = await browser.newPage()
   await page.goto('https://cv.hanneko.no')
-  const year = new Date().getFullYear()
+  var year = new Date().getFullYear()
   await page.pdf({
-    path: `cv_hanneko_${year}.pdf`,
+    path: 'cv_hanneko_' + year + '.pdf',
     format: 'A4',
     printBackground: true,
   })
